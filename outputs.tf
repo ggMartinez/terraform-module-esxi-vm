034f4fafe4a2b9e1d52602ea07b76a30b412915c
Gonzalo Martinez <gon.martinez@icloud.com>
2021-12-28 05:19:35 -0300

    Added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,22 @@
-output "VM" {
-  value = esxi_guest.vmguest
+output "Guest Name" {
+  value = esxi_guest.vmguest.guest_name
+}
+output "IP Address" {
+  value = esxi_guest.vmguest.ip_address
 }
 
+output "RAM" {
+  value = esxi_guest.vmguest.memsize
+}
+
+output "CPU Count" {
+  value = esxi_guest.vmguest.numvcpus
+}
+
+output "Guest OS" {
+  value = esxi_guest.vmguest.guestos
+}
+
+output "Template Name" {
+  value = esxi_guest.vmguest.clone_from_vm
+}
