b0aea5ebbb577e070bd33006612360618257ef74
Gonzalo Martinez <gon.martinez@icloud.com>
2021-12-28 04:50:13 -0300

    Minor fix

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "esxi_guest" "vmguest" {
 
   guestinfo = {
     "userdata.encoding" = "gzip+base64"
-    "userdata"          = base64gzip(format("%s%s",data.template_file.userdata_default.rendered,data.template_file.userdata.rendered)
+    "userdata"          = base64gzip(format("%s%s",data.template_file.userdata_default.rendered,data.template_file.userdata.rendered))
   }
 }
 
