3df1877263e659bd569ef6d0f28b5ae491f13dda
Gonzalo Martinez <gon.martinez@icloud.com>
2021-12-28 03:35:29 -0300

    Added default values

--- variables.tf
+++ variables.tf
@@ -4,31 +4,39 @@ variable "esxiHostname" {
 }
 
 variable "esxiSSHPort" {
+   default = "22"
 }
 
 variable "esxiSSLPort" {
+   default = "443"
 }
 
 variable "esxiUsername" {
+   default = "root"
 }
 
 variable "esxiPassword" {
 }
 
 variable "virtualNetwork" {
+   default = "VM Network"
 }
 
 variable "dataStore" {
+   default = "datastore1"
 }
 
 variable "templateVM" {
+   default = "Centos7"
 }
 
 variable "guestHostname" {
 }
 
 variable "cpuCount" {
+   default = "1"
 }
 
 variable "memSize" {
+   default = "1024"
 }
